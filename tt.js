d5f8baa5092439005cfedbad2ce3b0478e3ea88b
loupo233 <2531244783@qq.com>
2025-01-30 22:00:22 +0800

    Update tt.js

--- tt.js
+++ tt.js
@@ -1,36 +1,20 @@
-// ==UserScript==
-// @name         BC 自动封禁
-// @namespace    https://www.bondageprojects.com/
-// @version      0.3.0
-// @description  代码测试
-// @author       0 0
-// @include      /^https:\/\/(www\.)?bondageprojects\.elementfx\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
-// @include      /^https:\/\/(www\.)?bondage-europe\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
-// @grant        none
-// @license      MIT
-// ==/UserScript==
+if (typeof bcModSdk === "undefined") {
+    console.error("bcModSdk 未加载，请检查 mod-sdk.js 是否正确引入！");
+} else {
+    console.log("bcModSdk 版本:", bcModSdk.version);
 
-import { get, post } from "./son.js";
-
-(function ( bcModSdk ) {
-    'use strict';
-    // =======================================================================================
-    
-    const MOD_NAME = "自动封禁";
-    const MOD_FULL_NAME = "自动封禁";
-    const MOD_VERSION = "0.0.1";
-
-    const 笨蛋Luzi = bcModSdk.registerMod({
-        name: MOD_NAME,
-        fullName: MOD_FULL_NAME,
-        version: MOD_VERSION
+    // 注册 Mod
+    const myMod = bcModSdk.registerMod({
+        name: "测试",
+        fullName: "loupo的代码测试",
+        version: "0.0.1",
     });
-    // 钩子函数
-    笨蛋Luzi.hookFunction("ChatRoomMessage", 0, (args, next) => {
-        const data = args[0];  
-             console.log(data);  
-            return next(args);     
-      });
-    // ========================================================================
-    // ========================================================================
-})();
+
+    // Hook 一个函数（拦截）
+    myMod.hookFunction("ChatMessage", 10, (args, next) => {
+        console.log(player.name, args[0]); 
+        return next(args);
+    });
+
+    console.log("MyMod 加载完成！");
+}
