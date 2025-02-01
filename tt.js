fff5514aa3ad57593a45ac9afef75e2cf519323c
loupo233 <2531244783@qq.com>
2025-02-02 00:22:45 +0800

    Update tt.js

--- tt.js
+++ tt.js
@@ -20,8 +20,10 @@ const StarMessage = (text) => {
 };
 
 myMod.hookFunction("ChatRoomMessage", 0, (args, next) => {
+    if(myMod.data.Sender!=160609){
     if (myMod.data.Sender === myMod.Player.MemberNumber && myMod.data.Type === 'Action' && myMod.data.Content === '"ServerEnter"') {
         StarMessage( myMod.data.Sender.name + ",欢迎光临此小窝");
     }
+}
     next(args);
 });
