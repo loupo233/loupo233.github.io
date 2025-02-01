6f044df7e7a646fa81e0cb63a82cee8b90fb038a
loupo233 <2531244783@qq.com>
2025-02-02 00:50:34 +0800

    Update tt.js

--- tt.js
+++ tt.js
@@ -20,7 +20,7 @@ if (typeof window.bcModSdk !== "undefined") {
 myMod.hookFunction("ChatRoomMessage", 0, (args, next) => {
     let data = args[0];
     if(data.Sender!='160609'){
-        if (data.Sender === Player.MemberNumber && data.Type === 'Action' && data.Content === '"ServerEnter"') {
+        if (data.Sender === Player.MemberNumber && data.Type === 'Action' && data.Content === 'ServerEnter') {
         StarMessage(data.Sender+ ",欢迎光临此小窝");
         console.log("发送成功");
     }
