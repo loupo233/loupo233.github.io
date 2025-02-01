d549c6955c8bef7acb014948000aa8d0af54cda7
loupo233 <2531244783@qq.com>
2025-02-02 01:04:27 +0800

    Update tt.js

--- tt.js
+++ tt.js
@@ -17,16 +17,16 @@ const StarMessage = (text) => {
         Type: "Emote",
     })
 };
-myMod.hookFunction("ChatRoomMessage", 0, (args, next) => {
-    let data1 = args[0];
-    // Sender // 玩家名字 是数字类型
-    // Type // 数据类型 
-    // Content // 内容 聊天的话就是文字 动作的话就是动作名字
-    // Dictionary // 也是内容 通常用于替换
-    console.log(data1);
-    next(args);
-});
-myMod.hookFunction("ChatRoomMessage", 0, (args, next) => {
+// myMod.hookFunction("ChatRoomMessage", 0, (args, next) => {
+//     let data1 = args[0];
+//     // Sender // 玩家名字 是数字类型
+//     // Type // 数据类型 
+//     // Content // 内容 聊天的话就是文字 动作的话就是动作名字
+//     // Dictionary // 也是内容 通常用于替换
+//     console.log(data1);
+//     next(args);
+// });
+myMod.hookFunction("ChatRoomMessage", 10, (args, next) => {
     let data = args[0];
     if(data.Sender!='160609'){
 
@@ -34,9 +34,10 @@ myMod.hookFunction("ChatRoomMessage", 0, (args, next) => {
         StarMessage(data.Sender+ ",欢迎光临此小窝");
         console.log("发送成功");
     }
-    else {
-        console.log("本人测试成功");
-    }
+    
+}
+else {
+    console.log("本人测试成功");
 }
     next(args);
 });
