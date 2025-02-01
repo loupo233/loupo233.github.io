99921475348dab38cd368c49c6e6e7f79b216c9a
loupo233 <2531244783@qq.com>
2025-02-02 00:53:31 +0800

    Update tt.js

--- tt.js
+++ tt.js
@@ -17,12 +17,12 @@ if (typeof window.bcModSdk !== "undefined") {
     console.error("Mod SDK 未加载");
 }
 myMod.hookFunction("ChatRoomMessage", 0, (args, next) => {
-    let data = args[0];
+    let data1 = args[0];
     // Sender // 玩家名字 是数字类型
     // Type // 数据类型 
     // Content // 内容 聊天的话就是文字 动作的话就是动作名字
     // Dictionary // 也是内容 通常用于替换
-    console.log(data);
+    console.log(data1);
     next(args);
 });
 myMod.hookFunction("ChatRoomMessage", 0, (args, next) => {
