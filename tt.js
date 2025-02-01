844b04679e72bb41eeed00c7d5163891bfb62530
loupo233 <2531244783@qq.com>
2025-02-02 00:20:46 +0800

    Update tt.js

--- tt.js
+++ tt.js
@@ -11,30 +11,17 @@ if (typeof window.bcModSdk !== "undefined") {
 } else {
     console.error("Mod SDK 未加载");
 }
-myMod.hookFunction("ChatRoomMessage", 10, (args, next) => {
-    const messageData = args[0];  
-    
-    // 确保 messageData 存在并且是字符串
-    if (!messageData || typeof messageData !== "string") {
-        console.error("无效的消息数据:", messageData);
-        return next(args);
+
+const StarMessage = (text) => {
+    ServerSend("ChatRoomChat", {
+        Content: `${text}`,
+        Type: "Emote",
+    })
+};
+
+myMod.hookFunction("ChatRoomMessage", 0, (args, next) => {
+    if (myMod.data.Sender === myMod.Player.MemberNumber && myMod.data.Type === 'Action' && myMod.data.Content === '"ServerEnter"') {
+        StarMessage( myMod.data.Sender.name + ",欢迎光临此小窝");
     }
-
-    const joinRegex = /^(.+?) \[(.+?)\] 进来了\.$/;  
-    const match = messageData.match(joinRegex);
-
-    if (match) {
-        const nickname = match[1];
-        const name = match[2];
-
-        console.log(`检测到新用户: ${nickname} (${name})`);
-
-        // 发送欢迎消息
-        ServerSend("ChatRoomChat", {
-            Content: `欢迎 ${nickname} !`,
-            Type: "Emote",
-        });
-    }
-
-    return next(args);
+    next(args);
 });
