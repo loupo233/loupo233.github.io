8fa3615e06f1f80cc3ae71f3b8d02693744247bf
loupo233 <2531244783@qq.com>
2025-02-02 00:42:15 +0800

    Update tt.js

--- tt.js
+++ tt.js
@@ -18,9 +18,10 @@ if (typeof window.bcModSdk !== "undefined") {
 }
 
 myMod.hookFunction("ChatRoomMessage", 0, (args, next) => {
-    if(myMod.data.Sender!='160609'){
-        if (myMod.data.Sender === myMod.Player.MemberNumber && myMod.data.Type === 'Action' && myMod.data.Content === '"ServerEnter"') {
-        StarMessage( myMod.data.Sender.name + ",欢迎光临此小窝");
+    let data = args[0];
+    if(data.Sender!='160609'){
+        if (data.Sender === Player.MemberNumber && data.Type === 'Action' && data.Content === '"ServerEnter"') {
+        StarMessage(data.Sender+ ",欢迎光临此小窝");
     }
 }
     next(args);
