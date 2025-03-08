0cf9fe0fa7ee69c8ab485f2737f907039d7750b9
loupo233 <2531244783@qq.com>
2025-03-08 08:23:49 +0800

    Update tt.js

--- tt.js
+++ tt.js
@@ -17,36 +17,13 @@ const ChatMessage = (text) => {
         Type: "Chat",
     })
 };
-myMod.hookFunction("ChatRoomCharacter",0, (args, next) => {
-
-})
-// myMod.hookFunction("ChatRoomMessage", 0, (args, next) => {
-//     let data1 = args[0];
-//     // Sender // 玩家名字 是数字类型
-//     // Type // 数据类型 
-//     // Content // 内容 聊天的话就是文字 动作的话就是动作名字
-//     // Dictionary // 也是内容 通常用于替换
-//     console.log(data1);
-//     next(args);
-// });
-// ServerSend 这个会 发送给服务器 大家都可以看见
-// 括号消息 = (text) => {
-//     ServerSend("ChatRoomChat", {
-//         Content: "Beep",
-//         Type: "Action",
-//         Dictionary: [
-//             { Tag: "发送私聊", Text: "修改" }, // 这条必须有 不然会不显示 目的是为了修改 "发送私聊" 这个文本
-//             { Tag: "修改", Text: text },      // 修改 "发送私聊" 这个文本
-//         ],
-//     })
-// };
-myMod.hookFunction("ChatRoomMessage", 10, (args, next) => {
+myMod.hookFunction("ChatRoomMessage", 11, (args, next) => {
     let data = args[0];
      if(data.Sender!='160609'){
         // console.log(Player.MemberNumber);
         if (data.Sender  && data.Type === 'Action' && data.Content === 'ServerEnter') {
-         const  emdsa= ChatRoomCharacter.find(character => character.MemberNumber === data.Sender); // 这个是调出特定玩家的信息
-         ChatMessage(emdsa.name+ ",欢迎光临此小窝");
+         const  lgd= ChatRoomCharacter.find(character => character.MemberNumber === data.Sender); // 这个是调出特定玩家的信息
+         ChatMessage(lgd.AccountName+ ",欢迎光临此小窝");
     }
     
     }
