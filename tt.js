0132aaeb606c79e5c26710eb635f3ceec70bdbde
loupo233 <2531244783@qq.com>
2025-04-22 15:42:52 +0800

    Update tt.js

--- tt.js
+++ tt.js
@@ -27,7 +27,8 @@ myMod.hookFunction("ChatRoomMessage", 11, (args, next) => {
          const  dsa= ChatRoomCharacter.find(character => character.MemberNumber === data.Sender); 
         // 调用 CharacterNickname 得到格式化后的昵称
          const dasnickname = CharacterNickname(dsa);
-         ChatMessage(dasnickname+ "(https://raw.githubusercontent.com/loupo233/loupo233.github.io/refs/heads/main/112b1a83-728e-47a3-a6d7-814602314c83.png)");
+        //  ChatMessage(dasnickname+ "(https://raw.githubusercontent.com/loupo233/loupo233.github.io/refs/heads/main/112b1a83-728e-47a3-a6d7-814602314c83.png)");
+        ChatMessage("(https://raw.githubusercontent.com/loupo233/loupo233.github.io/refs/heads/main/112b1a83-728e-47a3-a6d7-814602314c83.png)");
         }
     
     } next(args);
